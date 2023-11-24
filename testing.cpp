e1b7822bc8c89100f06cb048453f952abec6a23f
Liam Tolkkinen <liamtolkki@gmail.com>
2023-11-23 23:30:00 -0500

    Added a testing line to test complex division

--- testing.cpp
+++ testing.cpp
@@ -1,5 +1,5 @@
 /*
-A testing environment for the math operators made by me
+A testing environment for the math library made by me
 Liam Tolkkinen
 11/23/2023
 */
@@ -17,18 +17,20 @@ long double largestValue = std::numeric_limits<long double>::max();
 int main()
 {
     printf("testing...\n");
-    printf("new complex: 5 + 19i:\n");
-    ComplexNum complexValue = ComplexNum(5, 19);
-    printf("toString() test: %s\n", complexValue.toString().c_str());
-
-    printf("multiplying 2 complex numbers together:\n(%f %c %fi) * (%f %c %fi):\n", multiplyRVal1,
-           (multiplyIVal1 >= 0 ? '+' : '-'), (multiplyIVal1 >= 0 ? multiplyIVal1 : multiplyIVal1 * -1),
-           multiplyRVal2, (multiplyIVal2 >= 0 ? '+' : '-'),
-           (multiplyIVal2 >= 0 ? multiplyIVal2 : multiplyIVal2 * -1));
     ComplexNum complexValue1 = ComplexNum(multiplyRVal1, multiplyIVal1);
     ComplexNum complexValue2 = ComplexNum(multiplyRVal2, multiplyIVal2);
+    printf("toString() test: %s\n", complexValue1.toString().c_str());
+
+    printf("multiplying 2 complex numbers together:\n%s * %s:\n",
+           complexValue1.toString().c_str(), complexValue2.toString().c_str());
+
     ComplexNum complexMultiplyResult = complexValue1 * complexValue2;
     printf("Result: %s\n", complexMultiplyResult.toString().c_str());
+
+    printf("dividing 2 complex numbers together:\n%s / %s:\n",
+           complexValue1.toString().c_str(), complexValue2.toString().c_str());
+    ComplexNum complexDivideResult = complexValue1 / complexValue2;
+    printf("Result: %s\n", complexDivideResult.toString().c_str());
 
     return 0;
 }
