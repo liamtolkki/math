840dcd7f8a113a8decd585483c494ab556b0f36c
Liam Tolkkinen <liamtolkki@gmail.com>
2023-11-23 21:32:01 -0500

    added complex numbers to the lib

---
+++ complexNum.cpp
@@ -0,0 +1,10 @@
+#include "math.h"
+
+imaginaryType ComplexNum::getImg()
+{
+    return img;
+}
+realType ComplexNum::getReal()
+{
+    return real;
+}
--- math.h
+++ math.h
@@ -6,11 +6,11 @@ liamtolkki@gmail.com
 A fun project to calculate certain math operations
 --basic
     --basic math functions
-        --log_n(x)
+        --log_n(base, x)
         --exp(x)
         --sqrt(x)
         --nthroot(n, x)
-
+    --complex numbers (EX: 3 + 5i)
         ...
 --Calculus
     --differentiation
@@ -25,9 +25,30 @@ A fun project to calculate certain math operations
     ...
 */
 
+typedef long double mathType;
+typedef double imaginaryType;
+typedef double realType;
 class BasicMath
 {
+
     // TODO
+};
+
+class ComplexNum
+{
+    /*
+    complex numbers have both a real and an imaginary component.
+    ex: 3 + 5i
+    */
+private:
+    double real;
+    double img;
+
+public:
+    void compexNum(realType real, imaginaryType img);
+    realType getReal();
+    imaginaryType getImg();
+    
 };
 
 class Calculus
