189ace8df24c545175ba6efda6d78d26746440a9
Liam Tolkkinen <liamtolkki@gmail.com>
2023-11-23 20:23:36 -0500

    Started adding the class names

---
+++ math.h
@@ -0,0 +1,41 @@
+/*
+----------------------
+Liam Tolkkinen
+liamtolkki@gmail.com
+----------------------
+A fun project to calculate certain math operations
+--basic
+    --basic math functions
+        --log_n(x)
+        --exp(x)
+        --sqrt(x)
+        --nthroot(n, x)
+
+        ...
+--Calculus
+    --differentiation
+    --integration
+    --sums
+    ...
+--Linear Algebra
+    --matrices
+        --determinants
+    --vector/matrix multiplication
+    --cross/dot product
+    ...
+*/
+
+class BasicMath
+{
+    // TODO
+};
+
+class Calculus
+{
+    // TODO
+};
+
+class LinearAlgebra
+{
+    // TODO
+};
