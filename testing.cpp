550ddafeb5d48a01e3cbb79a897b160a28c37616
Liam Tolkkinen <liamtolkki@gmail.com>
2023-11-24 17:48:07 -0500

    added new testing lines

--- testing.cpp
+++ testing.cpp
@@ -11,26 +11,32 @@ Liam Tolkkinen
 #define multiplyIVal1 2.0
 #define multiplyRVal2 0.0
 #define multiplyIVal2 1.0
+#define factorial 0
 // holds the largest long double possible
-long double largestValue = std::numeric_limits<long double>::max();
+decimalType largestValue = std::numeric_limits<decimalType>::max();
 
 int main()
 {
-    printf("testing...\n");
-    ComplexNum complexValue1 = ComplexNum(multiplyRVal1, multiplyIVal1);
-    ComplexNum complexValue2 = ComplexNum(multiplyRVal2, multiplyIVal2);
-    printf("toString() test: %s\n", complexValue1.toString().c_str());
+       printf("testing...\n");
+       ComplexNum complexValue1 = ComplexNum(multiplyRVal1, multiplyIVal1);
+       ComplexNum complexValue2 = ComplexNum(multiplyRVal2, multiplyIVal2);
+       printf("toString() test: %s\n", complexValue1.toString().c_str());
 
-    printf("multiplying 2 complex numbers together:\n%s * %s:\n",
-           complexValue1.toString().c_str(), complexValue2.toString().c_str());
+       printf("multiplying 2 complex numbers together:\n%s * %s:\n",
+              complexValue1.toString().c_str(), complexValue2.toString().c_str());
 
-    ComplexNum complexMultiplyResult = complexValue1 * complexValue2;
-    printf("Result: %s\n", complexMultiplyResult.toString().c_str());
+       ComplexNum complexMultiplyResult = complexValue1 * complexValue2;
+       printf("Result: %s\n", complexMultiplyResult.toString().c_str());
 
-    printf("dividing 2 complex numbers together:\n%s / %s:\n",
-           complexValue1.toString().c_str(), complexValue2.toString().c_str());
-    ComplexNum complexDivideResult = complexValue1 / complexValue2;
-    printf("Result: %s\n", complexDivideResult.toString().c_str());
+       printf("dividing 2 complex numbers together:\n%s / %s:\n",
+              complexValue1.toString().c_str(), complexValue2.toString().c_str());
+       ComplexNum complexDivideResult = complexValue1 / complexValue2;
+       printf("Result: %s\n", complexDivideResult.toString().c_str());
 
-    return 0;
+       printf("PI is: %Lf\ne is: %Lf\n", pi, e);
+       for (int i = 0; i < 30; i++)
+       {
+              printf("Testing factorial!\n%d!: %ld\n", factorial + i, fact(factorial + i));
+       }
+       return 0;
 }
