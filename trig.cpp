679154ffd3c4a84fceed13e9665df8a240faf7a2
Liam Tolkkinen <liamtolkki@gmail.com>
2023-11-27 01:25:03 -0500

    working cos and tan functions

--- trig.cpp
+++ trig.cpp
@@ -26,9 +26,27 @@ decimalType sin(decimalType x)
 }
 decimalType cos(decimalType x)
 {
+    decimalType sum = 0.0; // start at zero
+    x = fmod(x, 2 * PI);   // so that the series appx is more accurate (and faster)
+    // Taylor series:
+    for (int n = 0; n <= 20; n++) // 20 iterations
+    {
+        sum += (n % 2 == 0 ? 1 : -1) * pow(x, (2 * n)) / fact(2 * n);
+    }
+    return sum;
 }
 decimalType tan(decimalType x)
 {
+    decimalType cosX = cos(x);
+    if (cosX != 0)
+    {
+        return (sin(x) / cos(x));
+    }
+    else // divide by zero!
+    {
+        throw std::runtime_error("[DOMAIN ERROR] Tangent invalid input!\nCannot be a multiple of 1/2*PI or 3/4*PI!");
+        return 0;
+    }
 }
 decimalType sec(decimalType x)
 {
