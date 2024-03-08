29621e8a0af3ecf1b374a138c77fdb3abcc453de
Liam Tolkkinen <liamtolkki@gmail.com>
2024-03-08 18:22:28 -0500

    Moved trig functions from math.h to trig.h

--- include/math.h
+++ include/math.h
@@ -2,7 +2,6 @@
 #include <string>
 #include <stdexcept>
 #include <cstdio>
-#define TRIG
 // #define __MATRIX_INVERSE
 #define __SCIENTIFIC_NOTATION_CONSTANTS
 #define __VECTOR_NOTATION_CONSTANTS
@@ -13,10 +12,13 @@ typedef decimalType (*term)(double); // parses the terms of a function, ex: x^2 
 // defined 2 different epsilon values, depending on the function it is used for
 #define epsilon_TRIG (decimalType)(1.0E-10) // because there will always be error with appx
 #define epsilon_BASIC (decimalType)(1.0E-7)
+// useful constants
 const decimalType PI = 3.14159265358979;
 const decimalType e = 2.7182818284590452;
 const decimalType __SQRT_2 = 1.41421356237309504880;
 const decimalType __GOLDEN_RATIO = 1.61803398874989484821;
+
+// these are just for personal convention
 #ifdef __SCIENTIFIC_NOTATION_CONSTANTS
 const int STD_FORM = 0;
 const int SCI_FORM = 1;
@@ -49,31 +51,6 @@ decimalType nRoot(decimalType x, int n);
 decimalType sqrt(decimalType x);
 decimalType pow(decimalType x, int n);
 decimalType square(decimalType x);
-
-// BEGIN BASIC TRIG FUNCTIONS--------------
-#ifdef TRIG
-decimalType sin(decimalType x);
-decimalType cos(decimalType x);
-decimalType tan(decimalType x);
-decimalType sec(decimalType x);
-decimalType csc(decimalType x);
-decimalType cot(decimalType x);
-decimalType arcsin(decimalType x);
-decimalType arccos(decimalType x);
-decimalType arctan(decimalType x);
-decimalType arcsec(decimalType x);
-decimalType arccsc(decimalType x);
-decimalType arccot(decimalType x);
-decimalType toDeg(decimalType rad); // radians -> degrees
-decimalType toRad(decimalType deg); // degrees -> radians
-#endif
-// END BASIC TRIG FUNCTIONS-----------------
-
-class BasicMath
-{
-
-public:
-};
 
 class SciNot
 { // for scientific notation, ex: 9747582 = 9.747582E+6
