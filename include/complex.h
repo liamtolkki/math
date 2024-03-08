137183a8f08c8606eecad6cec4b9b2ea9f28954c
Liam Tolkkinen <liamtolkki@gmail.com>
2024-03-08 18:59:05 -0500

    Separated this structure into its own cpp file

---
+++ include/complex.h
@@ -0,0 +1,19 @@
+#include "math.h"
+class ComplexNum
+{
+    /*
+    complex numbers have both a real and an imaginary component.
+    ex: 3 + 5i
+    */
+private:
+    double real;
+    double img;
+
+public:
+    ComplexNum(realType real, imaginaryType img);
+    ComplexNum operator*(const ComplexNum &other) const; // overloads the * operator
+    ComplexNum operator/(const ComplexNum &other) const;
+    realType getReal();
+    imaginaryType getImg();
+    std::string toString();
+};
