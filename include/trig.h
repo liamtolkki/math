ad3f96366e68635494457a05fc82c3edf11194bd
Liam Tolkkinen <liamtolkki@gmail.com>
2024-03-08 18:22:48 -0500

    Moved trig functions

---
+++ include/trig.h
@@ -0,0 +1,20 @@
+#include "math.h"
+#define TRIG
+// BEGIN BASIC TRIG FUNCTIONS--------------
+#ifdef TRIG
+decimalType sin(decimalType x);
+decimalType cos(decimalType x);
+decimalType tan(decimalType x);
+decimalType sec(decimalType x);
+decimalType csc(decimalType x);
+decimalType cot(decimalType x);
+decimalType arcsin(decimalType x);
+decimalType arccos(decimalType x);
+decimalType arctan(decimalType x);
+decimalType arcsec(decimalType x);
+decimalType arccsc(decimalType x);
+decimalType arccot(decimalType x);
+decimalType toDeg(decimalType rad); // radians -> degrees
+decimalType toRad(decimalType deg); // degrees -> radians
+#endif
+// END BASIC TRIG FUNCTIONS-----------------
