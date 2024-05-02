93b4eb377f7f702ea8e7cf2534fe06bd7d62f239
Liam Tolkkinen <liamtolkki@gmail.com>
2024-05-02 00:10:58 -0400

    added the implemented methods
    still a lot to finish

--- src/polynomial.cpp
+++ src/polynomial.cpp
@@ -1,7 +1,21 @@
 #include "../include/polynomial.h"
 
-class Polynomial
+Polynomial::Polynomial()
 {
-public:
-private:
-};
+    coefficients = NULL;
+}
+Polynomial::Polynomial(decimalType *list, int sz)
+{
+    size = sz;
+    coefficients = new decimalType[sz]; // make hard copy
+    for (int i = 0; i < sz; i++)
+    {
+        coefficients[i] = list[i];
+    }
+}
+
+Polynomial::~Polynomial()
+{
+    if (coefficients)
+        delete[] coefficients;
+}
