d4f79fc2a1e81b5245c22de18529117c683ab17e
Liam Tolkkinen <liamtolkki@gmail.com>
2024-07-11 18:25:27 -0400

    started defining the declared functions

--- src/geometry.cpp
+++ src/geometry.cpp
@@ -2,10 +2,27 @@
 
 Polygon::Polygon()
 {
+    n = 0;
+    vertices = nullptr; // no space allocated
 }
-Polygon::Polygon(int n)
+Polygon::Polygon(int nVal)
 {
+    n = nVal;
+    vertices = new point[n]; // allocate the proper amount of space
 }
-Polygon::Polygon(int n, point *vertexArray)
+Polygon::Polygon(int nVal, point *vertexArray)
 {
+    n = nVal;
+    vertices = new point[n]; // allocate
+    for (int i = 0; i < n; i++)
+    {
+        vertices[i] = vertexArray[i]; // hard copy
+    }
+}
+Polygon::~Polygon()
+{
+    if (n > 0 && vertices != nullptr) // to prevent freeing a nullptr
+    {
+        delete[] vertices;
+    }
 }
