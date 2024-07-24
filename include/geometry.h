5a6a104e5fb79993e69a2eb002d741dd52ffa0a9
Liam Tolkkinen <liamtolkki@gmail.com>
2024-07-23 22:33:23 -0400

    added area() and perimeter() declaration and definitions

--- include/geometry.h
+++ include/geometry.h
@@ -3,7 +3,7 @@ Geometry header for geometry.cpp
 made by Liam Tolkkinen
 */
 #include "math.h"
-
+#ifdef __GEOMETRY
 struct point // represents a cartesian 2D point
 {
     point(decimalType x_val = 0.0, decimalType y_val = 0.0) : x(x_val), y(y_val) {}
@@ -22,8 +22,12 @@ public:
     void setN(int n);
     point *getVertices(); // returns an array of points
 
+    decimalType area();                // calculates and returns the area of the polygon
+                                       // uses Gauss's shoelace formula
+    decimalType perimeter();           // returns the perimeter of the polygon
     Polygon();                         // default constructor
     Polygon(int n);                    // default constructor given n
     Polygon(int n, point *vertexList); // constructor
     ~Polygon();                        // destructor
 };
+#endif
--- src/geometry.cpp
+++ src/geometry.cpp
@@ -1,5 +1,39 @@
 #include "../include/geometry.h"
 
+decimalType Polygon::area()
+{
+    decimalType area = 0.0;     // initialize the area to 0
+    for (int i = 0; i < n; i++) // n is the number of vertices
+    {
+        int j = (i + 1) % n;
+        area += vertices[i].x * vertices[j].y; // really just a big determinant calculation
+        area -= vertices[i].y * vertices[j].x;
+    }
+    area = abs(area / 2.0); // because the shoelace method calculates 2 * Area
+    return area;
+}
+
+decimalType distance(point p1, point p2)
+{ // returns the distance from p1 to p2
+    return sqrt(square(p1.x - p2.x) + square(p1.y - p2.y));
+}
+decimalType Polygon::perimeter()
+{
+    decimalType length = 0.0;
+    for (int i = 0; i < n; i++)
+    {
+        if (i == n - 1)
+        {
+            length += distance(vertices[i], vertices[0]); // wrap back to first point
+        }
+        else
+        {
+            length += distance(vertices[i], vertices[i + 1]); // sum distance
+        }
+    }
+    return length;
+}
+
 Polygon::Polygon()
 {
     n = 0;
