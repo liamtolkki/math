b384e73b1ed7b30ab2f1bdc2f3e26eb9c13fdaaa
Liam Tolkkinen <liamtolkki@gmail.com>
2024-10-23 15:30:45 -0400

    added an automatically generated header file
    this contains every header in the include directory except for itself

---
+++ include/allheaders.h
@@ -0,0 +1,25 @@
+// WARNING: THIS FILE IS AUTOMATICALLY
+// GENERATED USING ../scripts/headerGen
+// This is built from ../scripts/headerList.cpp
+// Contains all headers used in this library
+
+#pragma once
+#ifdef DEVELOPMENT_MODE
+#include "equation.h"
+#include "calculus.h"
+#include "polynomial.h"
+#include "geometry.h"
+#include "math.h"
+#include "complex.h"
+#include "trig.h"
+#include "linearAlgebra.h"
+#else
+#include <mathlib/equation.h>
+#include <mathlib/calculus.h>
+#include <mathlib/polynomial.h>
+#include <mathlib/geometry.h>
+#include <mathlib/math.h>
+#include <mathlib/complex.h>
+#include <mathlib/trig.h>
+#include <mathlib/linearAlgebra.h>
+#endif
